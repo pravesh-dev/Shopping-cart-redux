ad8517dd28d4fc23aaa3e0a4444224e15001fccf
pravesh dav <humantorch1932@gmail.com>
2024-05-27 17:01:09 +0530

    added item's detail page and shown items data. added increase and decrease quentity functionality and add to cart functionality from inside too. Shown total quantity of items added in header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import cartIcon from '../assets/images/cart.png'
+import { useSelector } from 'react-redux'
 
 function  Header() {
+  const [totalQuantity, setTotalQuantity] = useState(0)
+  const carts = useSelector(store => store.cart.items);
+  useEffect(()=>{
+    let total = 0;
+    carts.forEach(item => total += item.quantity);
+    setTotalQuantity(total)
+  }, [carts])
   return (
     <header className='flex justify-between items-center'>
       <Link to='/' className='text-xl font-semibold'>Home</Link>
       <div className='w-10 h-10 bg-gray-400 rounded-full flex justify-center items-center relative'>
         <img src={cartIcon} className='w-6' alt="" />
-        <span className='absolute top-2/3 right-1/2 bg-red-500 text-white text-sm w-5 h-5 rounded-full flex justify-center items-center'>0</span>
+        <span className='absolute top-2/3 right-1/2 bg-red-500 text-white text-sm w-5 h-5 rounded-full flex justify-center items-center'>{totalQuantity}</span>
       </div>
     </header>
   )
--- src/Pages/Detail.jsx
+++ src/Pages/Detail.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { products } from "../Products";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../stores/cart";
 
 function Detail() {
+  const { slug } = useParams();
+  const [detail, setDetail] = useState([]);
+  const [quantity, setQuantity] = useState(1);
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const findDetail = products.filter((product) => product.slug === slug);
+    if (findDetail.length > 0) {
+      setDetail(findDetail[0]);
+    } else {
+      window.location.href = "/";
+    }
+  }, [slug]);
+  const handleMinusQuantity = () =>{
+    setQuantity(quantity -1 < 1 ? 1 : quantity - 1);
+  }
+  const handleAddQuantity = () =>{
+    setQuantity(quantity + 1)
+  }
+  const handleAddToCart = () =>{
+    dispatch(addToCart({
+      productId: detail.id,
+      quantity: quantity
+    }))
+  }
   return (
     <div>
-      details
+      <h2 className="text-3xl text-center">Product Detail</h2>
+      <div className="grid grid-cols-2 gap-5 mt-5">
+        <div>
+          <img src={detail.image} className="w-full" alt="" />
+        </div>
+        <div className="flex flex-col gap-5">
+          <h1 className="text-4xl uppercase font-bold">{detail.name}</h1>
+          <p className="font-bold text-3xl">${detail.price}</p>
+          <div className="flex gap-5">
+            <div className="flex gap-2 justify-center items-center">
+              <button className="bg-gray-100 text-black h-full w-10 font-bold text-xl rounded-xl flex justify-center items-center" onClick={handleMinusQuantity}>
+                -
+              </button>
+              <button className="bg-gray-200 text-black h-full w-10 font-bold text-xl rounded-xl flex justify-center items-center">
+                {quantity}
+              </button>
+              <button className="bg-gray-100 text-black h-full w-10 font-bold text-xl rounded-xl flex justify-center items-center" onClick={handleAddQuantity}>
+                +
+              </button>
+            </div>
+            <button className="bg-slate-900 text-white px-7 py-3 rounded-xl shadow-2xl" onClick={handleAddToCart}>Add To Cart</button>
+          </div>
+          <p>{detail.description}</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Detail
+export default Detail;
