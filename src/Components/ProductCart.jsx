65f5c3eb8c2738ca60bee0dc1d1142ca8bdee690
pravesh dav <humantorch1932@gmail.com>
2024-05-27 16:28:30 +0530

    added add to cart functionality and if product already added then increase in quantity

--- src/Components/ProductCart.jsx
+++ src/Components/ProductCart.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import cartIcon from '../assets/images/cart.png'
+import { useSelector, useDispatch } from 'react-redux';
+import {addToCart} from '../stores/cart';
 
 function ProductCart(props) {
+    const carts = useSelector(store => store.cart.items)
+    console.log(carts)
     const {id, name, price, image, slug} = props.data;
+    const dispatch = useDispatch();
+    const handleAddToCart = () =>{
+        dispatch(addToCart({
+            productId: id,
+            quantity: 1
+        }))
+    }
   return (
     <div className='bg-white p-5 rounded-xl shadow-sm'>
       <Link to={slug}>
@@ -14,7 +25,7 @@ function ProductCart(props) {
         <p className='text-black text-2xl font-medium'>
             $<span className='text-black'>{price}</span>
         </p>
-        <button className='bg-gray-600 rounded-md text-sm hover:bg-gray-700 flex gap-2 px-2 py-1'>
+        <button className='bg-gray-600 rounded-md text-sm hover:bg-gray-700 flex gap-2 px-2 py-1' onClick={handleAddToCart}>
             <img src={cartIcon} alt="" className='w-5' />
             Add To Cart
         </button>
